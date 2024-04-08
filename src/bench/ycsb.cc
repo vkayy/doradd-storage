6c015b3b6e33577356d9d63cf249ce615a6939ad
Scofield626 <765203006@qq.com>
2024-04-08 21:04:38 +0000

    clean ycsb

--- src/bench/ycsb.cc
+++ src/bench/ycsb.cc
@@ -6,6 +6,30 @@
 #include "constants.hpp"
 #include "pipeline.hpp"
 
+#define GET_COWN(_INDEX) auto&& row##_INDEX = get_cown_ptr_from_addr<Row<YCSBRow>>(reinterpret_cast<void *>(txm->cown_ptrs[_INDEX]));
+#define GET_ROW(_INDEX) auto&& row##_INDEX = index->get_row(txm->indices[_INDEX]);
+#define TXN(_INDEX) \
+{ \
+  if (write_set_l & 0x1) \
+    memset(&acq_row##_INDEX->val, sum, WRITE_SIZE); \
+  else \
+  { \
+    for (int j = 0; j < ROW_SIZE; j++) \
+      sum += acq_row##_INDEX->val.payload[j]; \
+  } \
+  write_set_l >>= 1; \
+}
+#define M_LOG_LATENCY() \
+  { \
+    if constexpr (LOG_LATENCY) { \
+      auto time_now = std::chrono::system_clock::now(); \
+      std::chrono::duration<double> duration = time_now - init_time; \
+      uint32_t log_duration = static_cast<uint32_t>(duration.count() * 1'000'000); \
+      TxCounter::instance().log_latency(log_duration); \
+    } \
+    TxCounter::instance().incr(); \
+  }
+
 struct YCSBRow
 {
   char payload[ROW_SIZE];
@@ -23,16 +47,8 @@ static_assert(sizeof(YCSBTransactionMarshalled) == 128);
 struct YCSBTransaction
 {
 public:
-  // Bit field. Assume less than 16 concurrent rows per transaction
-  // uint16_t write_set;
-#ifdef LOG_LATENCY
-  // std::chrono::time_point<std::chrono::system_clock> init_time;
-#endif
   static Index<YCSBRow>* index;
-  static uint64_t cown_base_addr;
   
-//#ifdef INDEXER
-  // Indexer: read db and in-place update cown_ptr
   static int prepare_cowns(char* input)
   {
     auto txm = reinterpret_cast<YCSBTransactionMarshalled*>(input);
@@ -56,56 +72,6 @@ public:
     
     return sizeof(YCSBTransactionMarshalled);
   }
-//#else
-
-  // prefetch row entry before accessing in parse()
-  static int prepare_parse(const char* input)
-  {
-    const YCSBTransactionMarshalled* txm =
-      reinterpret_cast<const YCSBTransactionMarshalled*>(input);
-    
-    for (int i = 0; i < ROWS_PER_TX; i++)
-    {
-      auto* entry = index->get_row_addr(txm->indices[i]);
-      __builtin_prefetch(entry, 0, 1);
-    }
-
-    return sizeof(YCSBTransactionMarshalled);
-  }
-
-  #ifndef NO_IDX_LOOKUP
-  // prefetch cowns for when closure
-  static int prepare_process(const char* input)
-  {
-    const YCSBTransactionMarshalled* txm =
-      reinterpret_cast<const YCSBTransactionMarshalled*>(input);
-
-    for (int i = 0; i < ROWS_PER_TX; i++)
-    {
-      //auto* entry = index->get_row_addr(txm->indices[i]);
-      //auto&& cown = std::move(*entry);
-      auto&& cown = index->get_row(txm->indices[i]);
-      cown.prefetch();
-    }
-
-    return sizeof(YCSBTransactionMarshalled);
-  }
-  #else
-  static int prepare_process(const char* input, const int rw, const int locality)
-  {
-    const YCSBTransactionMarshalled* txm =
-      reinterpret_cast<const YCSBTransactionMarshalled*>(input);
-
-    for (int i = 0; i < ROWS_PER_TX; i++)
-    {
-      __builtin_prefetch(reinterpret_cast<const void *>(
-        cown_base_addr + (uint64_t)(1024 * txm->indices[i]) + 32), rw, locality);
-    }
-
-    return sizeof(YCSBTransactionMarshalled);
-  }
-  #endif // NO_IDX_LOOKUP
-//#endif // INDEXER
 
 #ifdef RPC_LATENCY
   static int parse_and_process(const char* input, ts_type init_time)
@@ -117,238 +83,30 @@ public:
       reinterpret_cast<const YCSBTransactionMarshalled*>(input);
 
     auto ws_cap = txm->write_set;
-#ifdef LOG_LATENCY
-    //tx.init_time = std::chrono::system_clock::now(); 
-#endif
 
 #if defined(INDEXER) || defined(TEST_TWO)
-    auto&& row0 = get_cown_ptr_from_addr<Row<YCSBRow>>(
-        reinterpret_cast<void *>(txm->cown_ptrs[0]));
-    auto&& row1 = get_cown_ptr_from_addr<Row<YCSBRow>>(
-        reinterpret_cast<void *>(txm->cown_ptrs[1]));
-    auto&& row2 = get_cown_ptr_from_addr<Row<YCSBRow>>(
-        reinterpret_cast<void *>(txm->cown_ptrs[2]));
-    auto&& row3 = get_cown_ptr_from_addr<Row<YCSBRow>>(
-        reinterpret_cast<void *>(txm->cown_ptrs[3]));
-    auto&& row4 = get_cown_ptr_from_addr<Row<YCSBRow>>(
-        reinterpret_cast<void *>(txm->cown_ptrs[4]));
-    auto&& row5 = get_cown_ptr_from_addr<Row<YCSBRow>>(
-        reinterpret_cast<void *>(txm->cown_ptrs[5]));
-    auto&& row6 = get_cown_ptr_from_addr<Row<YCSBRow>>(
-        reinterpret_cast<void *>(txm->cown_ptrs[6]));
-    auto&& row7 = get_cown_ptr_from_addr<Row<YCSBRow>>(
-        reinterpret_cast<void *>(txm->cown_ptrs[7]));
-    auto&& row8 = get_cown_ptr_from_addr<Row<YCSBRow>>(
-        reinterpret_cast<void *>(txm->cown_ptrs[8]));
-    auto&& row9 = get_cown_ptr_from_addr<Row<YCSBRow>>(
-        reinterpret_cast<void *>(txm->cown_ptrs[9]));
+    GET_COWN(0);GET_COWN(1);GET_COWN(2);GET_COWN(3);GET_COWN(4);
+    GET_COWN(5);GET_COWN(6);GET_COWN(7);GET_COWN(8);GET_COWN(9);
 #else
-  #ifdef NO_IDX_LOOKUP
-    auto&& row0 = get_cown_ptr_from_addr<Row<YCSBRow>>(
-          reinterpret_cast<void *>(cown_base_addr + (uint64_t)(1024 * txm->indices[0])));
-    auto&& row1 = get_cown_ptr_from_addr<Row<YCSBRow>>(
-          reinterpret_cast<void *>(cown_base_addr + (uint64_t)(1024 * txm->indices[1])));
-    auto&& row2 = get_cown_ptr_from_addr<Row<YCSBRow>>(
-          reinterpret_cast<void *>(cown_base_addr + (uint64_t)(1024 * txm->indices[2])));
-    auto&& row3 = get_cown_ptr_from_addr<Row<YCSBRow>>(
-          reinterpret_cast<void *>(cown_base_addr + (uint64_t)(1024 * txm->indices[3])));
-    auto&& row4 = get_cown_ptr_from_addr<Row<YCSBRow>>(
-          reinterpret_cast<void *>(cown_base_addr + (uint64_t)(1024 * txm->indices[4])));
-    auto&& row5 = get_cown_ptr_from_addr<Row<YCSBRow>>(
-          reinterpret_cast<void *>(cown_base_addr + (uint64_t)(1024 * txm->indices[5])));
-    auto&& row6 = get_cown_ptr_from_addr<Row<YCSBRow>>(
-          reinterpret_cast<void *>(cown_base_addr + (uint64_t)(1024 * txm->indices[6])));
-    auto&& row7 = get_cown_ptr_from_addr<Row<YCSBRow>>(
-          reinterpret_cast<void *>(cown_base_addr + (uint64_t)(1024 * txm->indices[7])));
-    auto&& row8 = get_cown_ptr_from_addr<Row<YCSBRow>>(
-          reinterpret_cast<void *>(cown_base_addr + (uint64_t)(1024 * txm->indices[8])));
-    auto&& row9 = get_cown_ptr_from_addr<Row<YCSBRow>>(
-          reinterpret_cast<void *>(cown_base_addr + (uint64_t)(1024 * txm->indices[9])));
-  #else
-    auto&& row0 = index->get_row(txm->indices[0]);
-    auto&& row1 = index->get_row(txm->indices[1]);
-    auto&& row2 = index->get_row(txm->indices[2]);
-    auto&& row3 = index->get_row(txm->indices[3]);
-    auto&& row4 = index->get_row(txm->indices[4]);
-    auto&& row5 = index->get_row(txm->indices[5]);
-    auto&& row6 = index->get_row(txm->indices[6]);
-    auto&& row7 = index->get_row(txm->indices[7]);
-    auto&& row8 = index->get_row(txm->indices[8]);
-    auto&& row9 = index->get_row(txm->indices[9]);
-  #endif // NO_IDX_LOOKUP
-#endif // INDEXER
+    GET_ROW(0);GET_ROW(1);GET_ROW(2);GET_ROW(3);GET_ROW(4);
+    GET_ROW(5);GET_ROW(6);GET_ROW(7);GET_ROW(8);GET_ROW(9);   
+#endif
 
-    using type1 = acquired_cown<Row<YCSBRow>>;
+    using AcqType = acquired_cown<Row<YCSBRow>>;
 #ifdef RPC_LATENCY
     when(row0,row1,row2,row3,row4,row5,row6,row7,row8,row9) << [ws_cap, init_time]
 #else
     when(row0,row1,row2,row3,row4,row5,row6,row7,row8,row9) << [ws_cap]
 #endif
-      (type1 acq_row0, type1 acq_row1, type1 acq_row2, type1 acq_row3, 
-       type1 acq_row4, type1 acq_row5, type1 acq_row6, type1 acq_row7,
-       type1 acq_row8, type1 acq_row9)
+      (AcqType acq_row0, AcqType acq_row1, AcqType acq_row2, AcqType acq_row3, 
+       AcqType acq_row4, AcqType acq_row5, AcqType acq_row6, AcqType acq_row7,
+       AcqType acq_row8, AcqType acq_row9)
     {
-#ifdef LOG_SCHED_OHEAD 
-      auto exec_init_time = std::chrono::system_clock::now(); 
-#endif
-//#ifdef ZERO_SERV_TIME 
-      // Null in txn logic
-
       uint8_t sum = 0;
       uint16_t write_set_l = ws_cap;
-//#else
-#if 0
-      auto process_each_row = [&sum](auto& ws, auto&& row) {
-        if (ws & 0x1)
-        {
-          memset(&row->val, sum, WRITE_SIZE);
-        }
-        else
-        {
-          for (int j = 0; j < ROW_SIZE; j++)
-            sum += row->val.payload[j];
-        }
-        ws >>= 1; 
-      };
-
-      auto process_rows = [&](auto&... acq_row) {
-        (process_each_row(write_set_l, acq_row), ...);
-      };
-
-      process_rows(acq_row0, acq_row1, acq_row2, acq_row3, acq_row4,
-          acq_row5, acq_row6, acq_row7, acq_row8, acq_row9);
-#endif
-#if 1 
       int j;
-      if (write_set_l & 0x1)
-      {
-        memset(&acq_row0->val, sum, WRITE_SIZE);
-      }
-      else
-      {
-        for (j = 0; j < ROW_SIZE; j++)
-          sum += acq_row0->val.payload[j];
-      }
-      write_set_l >>= 1;
-  
-      if (write_set_l & 0x1)
-      {
-        memset(&acq_row1->val, sum, WRITE_SIZE);
-      }
-      else
-      {
-        for (j = 0; j < ROW_SIZE; j++)
-          sum += acq_row1->val.payload[j];
-      }
-      write_set_l >>= 1;
-     
-      if (write_set_l & 0x1)
-      {
-        memset(&acq_row2->val, sum, WRITE_SIZE);
-      }
-      else
-      {
-        for (j = 0; j < ROW_SIZE; j++)
-          sum += acq_row2->val.payload[j];
-      }
-      write_set_l >>= 1;
-  
-      if (write_set_l & 0x1)
-      {
-        memset(&acq_row3->val, sum, WRITE_SIZE);
-      }
-      else
-      {
-        for (j = 0; j < ROW_SIZE; j++)
-          sum += acq_row3->val.payload[j];
-      }
-      write_set_l >>= 1;
-  
-      if (write_set_l & 0x1)
-      {
-        memset(&acq_row4->val, sum, WRITE_SIZE);
-      }
-      else
-      {
-        for (j = 0; j < ROW_SIZE; j++)
-          sum += acq_row4->val.payload[j];
-      }
-      write_set_l >>= 1;
-  
-      if (write_set_l & 0x1)
-      {
-        memset(&acq_row5->val, sum, WRITE_SIZE);
-      }
-      else
-      {
-        for (j = 0; j < ROW_SIZE; j++)
-          sum += acq_row5->val.payload[j];
-      }
-      write_set_l >>= 1;
-  
-      if (write_set_l & 0x1)
-      {
-        memset(&acq_row6->val, sum, WRITE_SIZE);
-      }
-      else
-      {
-        for (j = 0; j < ROW_SIZE; j++)
-          sum += acq_row6->val.payload[j];
-      }
-      write_set_l >>= 1;
-  
-      if (write_set_l & 0x1)
-      {
-        memset(&acq_row7->val, sum, WRITE_SIZE);
-      }
-      else
-      {
-        for (j = 0; j < ROW_SIZE; j++)
-          sum += acq_row7->val.payload[j];
-      }
-      write_set_l >>= 1;
-  
-      if (write_set_l & 0x1)
-      {
-        memset(&acq_row8->val, sum, WRITE_SIZE);
-      }
-      else
-      {
-        for (j = 0; j < ROW_SIZE; j++)
-          sum += acq_row8->val.payload[j];
-      }
-      write_set_l >>= 1;
-  
-      if (write_set_l & 0x1)
-      {
-        memset(&acq_row9->val, sum, WRITE_SIZE);
-      }
-      else
-      {
-        for (j = 0; j < ROW_SIZE; j++)
-          sum += acq_row9->val.payload[j];
-      }
-      write_set_l >>= 1;
-#endif
-
-      TxCounter::instance().incr();
-#ifdef LOG_LATENCY
-      auto time_now = std::chrono::system_clock::now();
-      //std::chrono::duration<double> duration = time_now - tx.init_time;
-      //std::chrono::duration<double> duration = time_now - exec_init_time;
-      std::chrono::duration<double> duration = time_now - init_time;
-      // log at precision - 1us
-      uint32_t log_duration = static_cast<uint32_t>(duration.count() * 1'000'000);
-  #ifdef LOG_SCHED_OHEAD
-      std::chrono::duration<double> duration_1 = time_now - exec_init_time;
-      uint32_t log_duration_1 = 
-        static_cast<uint32_t>(duration_1.count() * 10'000'000);
-      TxCounter::instance().log_latency(log_duration, log_duration_1);
-  #else
-      TxCounter::instance().log_latency(log_duration);
-  #endif
-#endif
-//#endif // ZERO_SERV_TIME
+      TXN(0);TXN(1);TXN(2);TXN(3);TXN(4);TXN(5);TXN(6);TXN(7);TXN(8);TXN(9);
+      M_LOG_LATENCY();
     };
     return sizeof(YCSBTransactionMarshalled);
   }
@@ -357,10 +115,6 @@ public:
 };
 
 Index<YCSBRow>* YCSBTransaction::index;
-uint64_t YCSBTransaction::cown_base_addr;
-std::unordered_map<std::thread::id, uint64_t*>* counter_map;
-std::unordered_map<std::thread::id, log_arr_type*>* log_map;
-std::mutex* counter_map_mutex;
 
 int main(int argc, char** argv)
 {
@@ -384,7 +138,6 @@ int main(int argc, char** argv)
   uint8_t* cown_arr_addr = static_cast<uint8_t*>(aligned_alloc_hpage(
         1024 * DB_SIZE));
 
-  YCSBTransaction::cown_base_addr = (uint64_t)cown_arr_addr;
   for (int i = 0; i < DB_SIZE; i++)
   {
     cown_ptr<Row<YCSBRow>> cown_r = make_cown_custom<Row<YCSBRow>>(
