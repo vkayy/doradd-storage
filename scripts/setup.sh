fcffca7d169a8741eb28588ae99116bee3639b9b
Scofield Liu <59379697+Scofield626@users.noreply.github.com>
2024-10-09 14:47:58 +0100

    add cpufrequtils

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,6 @@
 # install the dependencies
 sudo apt-get update
-sudo apt-get install -y pkg-config libssl-dev clang libclang-dev build-essential ninja-build cmake
+sudo apt-get install -y pkg-config libssl-dev clang libclang-dev build-essential ninja-build cmake cpufrequtils
 git submodule update --init
 
 # prepare txn logs
