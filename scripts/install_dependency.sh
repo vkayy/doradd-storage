ae3b2f9a02ea86f628083ac0837e7c178a95b649
Scofield Liu <59379697+Scofield626@users.noreply.github.com>
2024-11-24 22:21:04 +0000

    chore: update dependency script

--- scripts/install_dependency.sh
+++ scripts/install_dependency.sh
@@ -1,4 +1,5 @@
 # install the dependencies
 sudo apt-get update
-sudo apt-get install -y pkg-config libssl-dev clang libclang-dev build-essential ninja-build cmake cpufrequtils
+sudo apt-get install -y pkg-config libssl-dev clang libclang-dev build-essential ninja-build cmake cpufrequtils htop python3-pip
+pip install numpy
 git submodule update --init
